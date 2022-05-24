bf6e9d616465f636e53b0e32ec05b1bd4c822f76
Akin Karayun <akinkarayun1@hotmail.com>
2022-05-24 07:08:14 +0300

    cubic shape created and gave the functionality to rotate to each face

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="cube_container">
+      <input className='radio_button' type='radio' name='cube_gallery'></input>
+      <input className='radio_button' type='radio' name='cube_gallery'></input>
+      <input className='radio_button' type='radio' name='cube_gallery'></input>
+      <input className='radio_button' type='radio' name='cube_gallery'></input>
+      <input className='radio_button' type='radio' name='cube_gallery'></input>
+      <input className='radio_button' type='radio' name='cube_gallery'></input>
+
+
+      <div className="cube">
+        <div className="cube_side"></div>
+        <div className="cube_side"></div>
+        <div className="cube_side"></div>
+        <div className="cube_side"></div>
+        <div className="cube_side"></div>
+        <div className="cube_side"></div>
+
+
+      </div>
+
     </div>
   );
 }
